339109423a63273d7663d7d43bccd47a5a4c983c
iamsimranjot <iamsimranjot@gmail.com>
2016-10-22 16:16:49 +0530

    Initial Commit

---
+++ MemeMe 2.0/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe 2.0
+//
+//  Created by SimranJot Singh on 22/10/16.
+//  Copyright © 2016 SimranJot Singh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: NSString!
+    var bottomText: NSString!
+    var image: UIImage!
+    var memedImage: UIImage!
+}
+
